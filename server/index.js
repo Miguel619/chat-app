c930b648ff65dc9a01a586623f592b425451e7e9
Miguel619 <66089524+Miguel619@users.noreply.github.com>
2021-10-03 22:58:48 -0700

    Added messaging functionality between client and server 1.16

---
+++ server/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const http = require('http');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router')
+
+const app = express();
+
+const server = http.createServer(app);
+const io = require('socket.io')(server,{
+    cors:{
+        origin:'*',
+    }
+});
+
+io.on('connection', (socket) => {
+    socket.on('join', ({name, room}, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+        
+        if(error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`});
+
+        socket.join(user.room);
+
+        callback();
+
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, test: message});
+
+        callback();
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User Left');
+    })
+});
+
+
+app.use(router);
+
+
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
